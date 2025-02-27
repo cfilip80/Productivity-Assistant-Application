f257dc1966064fb4f8a81f88cff41e7cd5f798b5
Intranik <intranik.khazro@hotmail.com>
2025-02-27 02:47:24 +0100

    consol log data from form and create a clear fuction to clear data from form and out it in the array of object

--- script/script.js
+++ script/script.js
@@ -1,1 +1,46 @@
+const todoTitle = document.getElementById("todo-title");
+const todoCategory = document.getElementById("category");
+const todoTimeEstimate = document.getElementById("todo-time-estimate");
+const todoDeadline = document.getElementById("todo-deadline");
+const todoDescription = document.getElementById("description");
+const todoDoneBox = document.getElementById("todo-done-status");
+const todoNotDoneBox = document.getElementById("todo-notDone-status");
+const form = document.querySelector(".todo-form");
+const todosAndActivitis= []
+
+
+const todoClearForm = () => {
+    todoTitle.value = '';
+    todoCategory.value = '';
+    todoTimeEstimate.value = '';
+    todoDeadline.value = '';
+    todoDescription.value = '';
+    todoDoneBox.checked = false;
+    todoNotDoneBox.checked = false;
+}
+//function to todoDoneBox
+// let todoIsChecked = () => {
+//     if (todoDoneBox.checked)
+//         return todoDoneBox.value;
+//     else if(todoNotDoneBox.checked)
+//         return todoNotDoneBox.value;
+// }
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault(); // Förhindra att sidan laddas om
+    
+    const todoData = {
+        title: todoTitle.value,
+        category: todoCategory.value,
+        timeEstimate: todoTimeEstimate.value,
+        deadline: todoDeadline.value,
+        description: todoDescription.value, 
+        statustoe: todoDoneBox.checked ? "Done" : todoNotDoneBox.checked ? "Not Done" : "No status selected".value
+        
+    };
+    todosAndActivitis.push(todoData);
+    console.log("New Todo:", todoData);
+    console.log(todosAndActivitis);
+    todoClearForm();
+});
 
